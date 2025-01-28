ba906f85dd4fd8189c8d74fb3b12694512c013f2
jmot4 <jmota@gtech-services.com>
2025-01-28 15:28:38 -0600

    Agregar archivos de proyecto.

---
+++ MiPrimerApp2025C/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace MiPrimerApp2025C
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtNombre = new TextBox();
+            label1 = new Label();
+            btnEnviar = new Button();
+            SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(28, 41);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(358, 31);
+            txtNombre.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(28, 13);
+            label1.Name = "label1";
+            label1.Size = new Size(78, 25);
+            label1.TabIndex = 1;
+            label1.Text = "Nombre";
+            // 
+            // btnEnviar
+            // 
+            btnEnviar.Location = new Point(412, 41);
+            btnEnviar.Name = "btnEnviar";
+            btnEnviar.Size = new Size(112, 34);
+            btnEnviar.TabIndex = 2;
+            btnEnviar.Text = "Enviar";
+            btnEnviar.UseVisualStyleBackColor = true;
+            btnEnviar.Click += btnEnviar_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(547, 98);
+            Controls.Add(btnEnviar);
+            Controls.Add(label1);
+            Controls.Add(txtNombre);
+            Name = "Form1";
+            Text = "Mi Primer Aplicacion 2025";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtNombre;
+        private Label label1;
+        private Button btnEnviar;
+    }
+}
---
+++ MiPrimerApp2025C/Form1.cs
@@ -0,0 +1,15 @@
+namespace MiPrimerApp2025C
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(txtNombre.Text);
+        }
+    }
+}
