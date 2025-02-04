54ded6b96764d8bffd6a4a6726b0d0e1436f1bc1
arraydes <joseataydeleyva@gmail.com>
2025-02-04 15:48:37 -0600

    Cambio al 4/Febrero/2025, se añade el formulario menú, y el formulario de operaciones con 2 valores.

---
+++ MiPrimerApp2025C/FrmMenu.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace MiPrimerApp2025C
+{
+    partial class FrmMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            archivoToolStripMenuItem = new ToolStripMenuItem();
+            salirToolStripMenuItem = new ToolStripMenuItem();
+            aplicacionesToolStripMenuItem = new ToolStripMenuItem();
+            aplicaciónSimpleToolStripMenuItem = new ToolStripMenuItem();
+            operacionesDe2ValoresToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { archivoToolStripMenuItem, aplicacionesToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(483, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            archivoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { salirToolStripMenuItem });
+            archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            archivoToolStripMenuItem.Size = new Size(60, 20);
+            archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // salirToolStripMenuItem
+            // 
+            salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            salirToolStripMenuItem.Size = new Size(96, 22);
+            salirToolStripMenuItem.Text = "Salir";
+            salirToolStripMenuItem.Click += salirToolStripMenuItem_Click;
+            // 
+            // aplicacionesToolStripMenuItem
+            // 
+            aplicacionesToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { aplicaciónSimpleToolStripMenuItem, operacionesDe2ValoresToolStripMenuItem });
+            aplicacionesToolStripMenuItem.Name = "aplicacionesToolStripMenuItem";
+            aplicacionesToolStripMenuItem.Size = new Size(86, 20);
+            aplicacionesToolStripMenuItem.Text = "Aplicaciones";
+            aplicacionesToolStripMenuItem.Click += aplicacionesToolStripMenuItem_Click;
+            // 
+            // aplicaciónSimpleToolStripMenuItem
+            // 
+            aplicaciónSimpleToolStripMenuItem.Name = "aplicaciónSimpleToolStripMenuItem";
+            aplicaciónSimpleToolStripMenuItem.Size = new Size(205, 22);
+            aplicaciónSimpleToolStripMenuItem.Text = "Aplicación Simple";
+            aplicaciónSimpleToolStripMenuItem.Click += aplicaciónSimpleToolStripMenuItem_Click;
+            // 
+            // operacionesDe2ValoresToolStripMenuItem
+            // 
+            operacionesDe2ValoresToolStripMenuItem.Name = "operacionesDe2ValoresToolStripMenuItem";
+            operacionesDe2ValoresToolStripMenuItem.Size = new Size(205, 22);
+            operacionesDe2ValoresToolStripMenuItem.Text = "Operaciones de 2 valores";
+            operacionesDe2ValoresToolStripMenuItem.Click += operacionesDe2ValoresToolStripMenuItem_Click;
+            // 
+            // FrmMenu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(483, 426);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "FrmMenu";
+            Text = "Menú de aplicaciones en clase";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void aplicaciónSimpleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 AppSimple = new Form1();
+            AppSimple.Show();
+
+        }
+
+        private void aplicacionesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem archivoToolStripMenuItem;
+        private ToolStripMenuItem salirToolStripMenuItem;
+        private ToolStripMenuItem aplicacionesToolStripMenuItem;
+        private ToolStripMenuItem aplicaciónSimpleToolStripMenuItem;
+        private ToolStripMenuItem operacionesDe2ValoresToolStripMenuItem;
+    }
+}
---
+++ MiPrimerApp2025C/FrmMenu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiPrimerApp2025C
+{
+    public partial class FrmMenu : Form
+    {
+        public FrmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void operacionesDe2ValoresToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmOperacion2Numeros AppOperacionesCon2Valores = new FrmOperacion2Numeros();
+            AppOperacionesCon2Valores.Show();
+        }
+    }
+}
