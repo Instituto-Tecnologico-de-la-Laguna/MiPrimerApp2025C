e5c5a62343c931bc44fb3c1348edcab88d67ab1a
gerardordz1 <Jesus_ArreolaRodriguez@hotmail.com>
2025-02-05 15:44:50 -0600

    Se agregan cambios, se agrego un groupbox, manejo de excepciones el día 05/02/2025

--- MiPrimerApp2025C/frmOperacion2Numeros.Designer.cs
+++ MiPrimerApp2025C/frmOperacion2Numeros.Designer.cs
@@ -33,6 +33,13 @@
             textValor2 = new TextBox();
             label2 = new Label();
             btnCalcular = new Button();
+            groupBox1 = new GroupBox();
+            rdbDivision = new RadioButton();
+            rdbMultiplicacion = new RadioButton();
+            rdbResta = new RadioButton();
+            rdbSuma = new RadioButton();
+            btnBorrar = new Button();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
@@ -77,11 +84,80 @@
             btnCalcular.UseVisualStyleBackColor = true;
             btnCalcular.Click += btnCalcular_Click;
             // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(rdbDivision);
+            groupBox1.Controls.Add(rdbMultiplicacion);
+            groupBox1.Controls.Add(rdbResta);
+            groupBox1.Controls.Add(rdbSuma);
+            groupBox1.Location = new Point(24, 177);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(286, 125);
+            groupBox1.TabIndex = 5;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Operacion";
+            // 
+            // rdbDivision
+            // 
+            rdbDivision.AutoSize = true;
+            rdbDivision.Location = new Point(154, 70);
+            rdbDivision.Name = "rdbDivision";
+            rdbDivision.Size = new Size(83, 24);
+            rdbDivision.TabIndex = 3;
+            rdbDivision.TabStop = true;
+            rdbDivision.Text = "Division";
+            rdbDivision.UseVisualStyleBackColor = true;
+            // 
+            // rdbMultiplicacion
+            // 
+            rdbMultiplicacion.AutoSize = true;
+            rdbMultiplicacion.Location = new Point(154, 40);
+            rdbMultiplicacion.Name = "rdbMultiplicacion";
+            rdbMultiplicacion.Size = new Size(124, 24);
+            rdbMultiplicacion.TabIndex = 2;
+            rdbMultiplicacion.TabStop = true;
+            rdbMultiplicacion.Text = "Multiplicacion";
+            rdbMultiplicacion.UseVisualStyleBackColor = true;
+            // 
+            // rdbResta
+            // 
+            rdbResta.AutoSize = true;
+            rdbResta.Location = new Point(11, 70);
+            rdbResta.Name = "rdbResta";
+            rdbResta.Size = new Size(66, 24);
+            rdbResta.TabIndex = 1;
+            rdbResta.TabStop = true;
+            rdbResta.Text = "Resta";
+            rdbResta.UseVisualStyleBackColor = true;
+            // 
+            // rdbSuma
+            // 
+            rdbSuma.AutoSize = true;
+            rdbSuma.Location = new Point(11, 40);
+            rdbSuma.Name = "rdbSuma";
+            rdbSuma.Size = new Size(67, 24);
+            rdbSuma.TabIndex = 0;
+            rdbSuma.TabStop = true;
+            rdbSuma.Text = "Suma";
+            rdbSuma.UseVisualStyleBackColor = true;
+            // 
+            // btnBorrar
+            // 
+            btnBorrar.Location = new Point(291, 145);
+            btnBorrar.Name = "btnBorrar";
+            btnBorrar.Size = new Size(94, 29);
+            btnBorrar.TabIndex = 6;
+            btnBorrar.Text = "Borrar";
+            btnBorrar.UseVisualStyleBackColor = true;
+            btnBorrar.Click += btnBorrar_Click;
+            // 
             // frmOperacion2Numeros
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(448, 237);
+            ClientSize = new Size(480, 371);
+            Controls.Add(btnBorrar);
+            Controls.Add(groupBox1);
             Controls.Add(btnCalcular);
             Controls.Add(textValor2);
             Controls.Add(label2);
@@ -89,6 +165,8 @@
             Controls.Add(label1);
             Name = "frmOperacion2Numeros";
             Text = "Operaciones 2 Numeros";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -100,5 +178,11 @@
         private TextBox textValor2;
         private Label label2;
         private Button btnCalcular;
+        private GroupBox groupBox1;
+        private RadioButton rdbSuma;
+        private RadioButton rdbDivision;
+        private RadioButton rdbMultiplicacion;
+        private RadioButton rdbResta;
+        private Button btnBorrar;
     }
 }
--- MiPrimerApp2025C/frmOperacion2Numeros.cs
+++ MiPrimerApp2025C/frmOperacion2Numeros.cs
@@ -20,10 +20,33 @@ namespace MiPrimerApp2025C
         private void btnCalcular_Click(object sender, EventArgs e)
         {
             Double valor1, valor2;
-            valor1 = Convert.ToDouble(textValor1.Text); 
-            valor2 = double.Parse(textValor2.Text);
+            try
+            {
+                valor1 = Convert.ToDouble(textValor1.Text);
+                valor2 = double.Parse(textValor2.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ingrese valores numericos");
+                return;
+            }
 
-            MessageBox.Show("La suma de los valores es: " + (valor1 + valor2));
+            if (rdbSuma.Checked == true)
+                MessageBox.Show("La suma de los valres es: " + (valor1 + valor2));
+            else if (rdbResta.Checked == true)
+                MessageBox.Show("La suma de los valores es: " + (valor1 - valor2));
+            else if (rdbMultiplicacion.Checked == true)
+                MessageBox.Show("La multiplicacion es: " + (valor1 * valor2));
+            else
+                MessageBox.Show("La division es: " + (valor1 / valor2));
+
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            textValor1.Clear();
+            textValor2.Clear();
+            textValor1.Focus();
         }
     }
 }
