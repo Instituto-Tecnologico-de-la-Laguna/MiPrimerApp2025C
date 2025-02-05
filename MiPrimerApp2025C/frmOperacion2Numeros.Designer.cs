c0a913a2d7b8b77b1b338fe5cad34988d1231054
lv15-540870 <luisantoniocuevasortizpvc@gmail.com>
2025-02-05 15:43:11 -0600

    Cambios de clase 05/02/2025 Se agrega seleccion de operacion con radio button y se añade manejo de excepciones

--- MiPrimerApp2025C/frmOperacion2Numeros.Designer.cs
+++ MiPrimerApp2025C/frmOperacion2Numeros.Designer.cs
@@ -33,6 +33,13 @@
             txtValor2 = new TextBox();
             label2 = new Label();
             btnCalcular = new Button();
+            groupBox1 = new GroupBox();
+            rdbMultiplicacion = new RadioButton();
+            rdbDivision = new RadioButton();
+            rdbResta = new RadioButton();
+            rdbSuma = new RadioButton();
+            buttonBorrar = new Button();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
@@ -70,7 +77,7 @@
             // 
             // btnCalcular
             // 
-            btnCalcular.Location = new Point(334, 97);
+            btnCalcular.Location = new Point(334, 61);
             btnCalcular.Name = "btnCalcular";
             btnCalcular.Size = new Size(164, 29);
             btnCalcular.TabIndex = 4;
@@ -78,11 +85,80 @@
             btnCalcular.UseVisualStyleBackColor = true;
             btnCalcular.Click += btnCalcular_Click;
             // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(rdbMultiplicacion);
+            groupBox1.Controls.Add(rdbDivision);
+            groupBox1.Controls.Add(rdbResta);
+            groupBox1.Controls.Add(rdbSuma);
+            groupBox1.Location = new Point(30, 221);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(293, 125);
+            groupBox1.TabIndex = 5;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Operacion";
+            // 
+            // rdbMultiplicacion
+            // 
+            rdbMultiplicacion.AutoSize = true;
+            rdbMultiplicacion.Location = new Point(126, 38);
+            rdbMultiplicacion.Name = "rdbMultiplicacion";
+            rdbMultiplicacion.Size = new Size(124, 24);
+            rdbMultiplicacion.TabIndex = 3;
+            rdbMultiplicacion.TabStop = true;
+            rdbMultiplicacion.Text = "Multiplicacion";
+            rdbMultiplicacion.UseVisualStyleBackColor = true;
+            // 
+            // rdbDivision
+            // 
+            rdbDivision.AutoSize = true;
+            rdbDivision.Location = new Point(126, 86);
+            rdbDivision.Name = "rdbDivision";
+            rdbDivision.Size = new Size(83, 24);
+            rdbDivision.TabIndex = 2;
+            rdbDivision.TabStop = true;
+            rdbDivision.Text = "Division";
+            rdbDivision.UseVisualStyleBackColor = true;
+            // 
+            // rdbResta
+            // 
+            rdbResta.AutoSize = true;
+            rdbResta.Location = new Point(8, 86);
+            rdbResta.Name = "rdbResta";
+            rdbResta.Size = new Size(66, 24);
+            rdbResta.TabIndex = 1;
+            rdbResta.TabStop = true;
+            rdbResta.Text = "Resta";
+            rdbResta.UseVisualStyleBackColor = true;
+            // 
+            // rdbSuma
+            // 
+            rdbSuma.AutoSize = true;
+            rdbSuma.Location = new Point(8, 38);
+            rdbSuma.Name = "rdbSuma";
+            rdbSuma.Size = new Size(67, 24);
+            rdbSuma.TabIndex = 0;
+            rdbSuma.TabStop = true;
+            rdbSuma.Text = "Suma";
+            rdbSuma.UseVisualStyleBackColor = true;
+            // 
+            // buttonBorrar
+            // 
+            buttonBorrar.Location = new Point(334, 158);
+            buttonBorrar.Name = "buttonBorrar";
+            buttonBorrar.Size = new Size(164, 29);
+            buttonBorrar.TabIndex = 6;
+            buttonBorrar.Text = "Borrar";
+            buttonBorrar.UseVisualStyleBackColor = true;
+            buttonBorrar.Click += buttonBorrar_Click;
+            // 
             // frmOperacion2Numeros
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(755, 392);
+            ClientSize = new Size(638, 420);
+            Controls.Add(buttonBorrar);
+            Controls.Add(groupBox1);
             Controls.Add(btnCalcular);
             Controls.Add(txtValor2);
             Controls.Add(label2);
@@ -90,6 +166,8 @@
             Controls.Add(label1);
             Name = "frmOperacion2Numeros";
             Text = "Operaciones 2 Numeros";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -101,5 +179,11 @@
         private TextBox txtValor2;
         private Label label2;
         private Button btnCalcular;
+        private GroupBox groupBox1;
+        private RadioButton rdbMultiplicacion;
+        private RadioButton rdbDivision;
+        private RadioButton rdbResta;
+        private RadioButton rdbSuma;
+        private Button buttonBorrar;
     }
 }
--- MiPrimerApp2025C/frmOperacion2Numeros.cs
+++ MiPrimerApp2025C/frmOperacion2Numeros.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -25,9 +27,40 @@ namespace MiPrimerApp2025C
         private void btnCalcular_Click(object sender, EventArgs e)
         {
             Double valor1, valor2;
+        try { 
             valor1 = Convert.ToDouble(txtValor1.Text);
             valor2 = double.Parse(txtValor2.Text);
-            MessageBox.Show("La suma de los valores es: " + (valor1 + valor2));
+        }
+        catch(Exception)
+        {
+            MessageBox.Show("Ingrese valores numericos");
+            return;
+        }
+            if (rdbSuma.Checked == true)
+            {
+                MessageBox.Show("La suma de los valores es: " + (valor1 + valor2));
+            }
+            else if (rdbResta.Checked == true)
+            {
+                MessageBox.Show("La resta de los valores es: " + (valor1 - valor2));
+            }
+            else if (rdbMultiplicacion.Checked == true)
+            {
+                MessageBox.Show("La multiplicacion de los valores es: " + (valor1 * valor2));
+            }
+            else if (rdbDivision.Checked == true)
+            {
+                MessageBox.Show("La division de los valores es: " + (valor1 / valor2));
+            }
+
+
+
+        }
+
+        private void buttonBorrar_Click(object sender, EventArgs e)
+        {
+            txtValor1.Clear();
+            txtValor2.Clear();
         }
     }
 }
