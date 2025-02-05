b3980b6187314ceb309230399610ac3e5815d181
jmot4 <jmota@gtech-services.com>
2025-02-05 15:43:06 -0600

    Cambios de clase 05022025 Se agregar seleccion de operación con radioButton y se añade manejo de Excepciones.

--- MiPrimerApp2025C/frmOperacion2Numeros.cs
+++ MiPrimerApp2025C/frmOperacion2Numeros.cs
@@ -20,9 +20,33 @@ namespace MiPrimerApp2025C
         private void btnCalcular_Click(object sender, EventArgs e)
         {
             Double valor1, valor2;
-            valor1 = Convert.ToDouble(txtValor1.Text);
-            valor2= double.Parse(txtValor2.Text);
-            MessageBox.Show("La suma de los valores es: " + (valor1 + valor2));
+
+            try
+            {
+                valor1 = Convert.ToDouble(txtValor1.Text);
+                valor2 = double.Parse(txtValor2.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ingrese valores numericos");
+                return;
+            }
+
+            if (rdbSuma.Checked == true)
+                MessageBox.Show("La suma de los valores es: " + (valor1 + valor2));
+            else if (rdbResta.Checked == true)
+                MessageBox.Show("La resta de los valores es: " + (valor1 - valor2));
+            else if (rdbMultiplicacion.Checked == true)
+                MessageBox.Show("La multiplicacion de los valores es: " + (valor1 * valor2));
+            else
+                MessageBox.Show("La division de los valores es: " + (valor1 / valor2));
+
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            txtValor1 .Clear();
+            txtValor2.Clear();
         }
     }
 }
