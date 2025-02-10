54898f144685e36cd0e36bdb59433242366d5550
Angel Arroyo <angel.torres.08@hotmail.com>
2025-02-10 15:44:14 -0600

    Se agrego un lector de textos

---
+++ MiPrimerApp2025C/frmEditor.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace MiPrimerApp2025C
+{
+    partial class frmEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuEditor1 = new MenuStrip();
+            menuEditor = new ToolStripMenuItem();
+            nuevoToolStripMenuItem = new ToolStripMenuItem();
+            abrirToolStripMenuItem = new ToolStripMenuItem();
+            guardarToolStripMenuItem = new ToolStripMenuItem();
+            guardarComoToolStripMenuItem = new ToolStripMenuItem();
+            salirToolStripMenuItem = new ToolStripMenuItem();
+            richTextBox1 = new RichTextBox();
+            openFileDialogEditor = new OpenFileDialog();
+            saveFileDialogEditor = new SaveFileDialog();
+            menuEditor1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuEditor1
+            // 
+            menuEditor1.ImageScalingSize = new Size(20, 20);
+            menuEditor1.Items.AddRange(new ToolStripItem[] { menuEditor });
+            menuEditor1.Location = new Point(0, 0);
+            menuEditor1.Name = "menuEditor1";
+            menuEditor1.Size = new Size(800, 28);
+            menuEditor1.TabIndex = 0;
+            menuEditor1.Text = "menuEditor";
+            // 
+            // menuEditor
+            // 
+            menuEditor.DropDownItems.AddRange(new ToolStripItem[] { nuevoToolStripMenuItem, abrirToolStripMenuItem, guardarToolStripMenuItem, guardarComoToolStripMenuItem, salirToolStripMenuItem });
+            menuEditor.Name = "menuEditor";
+            menuEditor.Size = new Size(73, 24);
+            menuEditor.Text = "Archivo";
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            nuevoToolStripMenuItem.Size = new Size(224, 26);
+            nuevoToolStripMenuItem.Text = "Nuevo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            abrirToolStripMenuItem.Size = new Size(224, 26);
+            abrirToolStripMenuItem.Text = "Abrir";
+            abrirToolStripMenuItem.Click += abrirToolStripMenuItem_Click;
+            // 
+            // guardarToolStripMenuItem
+            // 
+            guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            guardarToolStripMenuItem.Size = new Size(224, 26);
+            guardarToolStripMenuItem.Text = "Guardar";
+            guardarToolStripMenuItem.Click += guardarToolStripMenuItem_Click;
+            // 
+            // guardarComoToolStripMenuItem
+            // 
+            guardarComoToolStripMenuItem.Name = "guardarComoToolStripMenuItem";
+            guardarComoToolStripMenuItem.Size = new Size(224, 26);
+            guardarComoToolStripMenuItem.Text = "Guardar como...";
+            // 
+            // salirToolStripMenuItem
+            // 
+            salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            salirToolStripMenuItem.Size = new Size(224, 26);
+            salirToolStripMenuItem.Text = "Salir";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Dock = DockStyle.Fill;
+            richTextBox1.Location = new Point(0, 28);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(800, 422);
+            richTextBox1.TabIndex = 1;
+            richTextBox1.Text = "";
+            richTextBox1.TextChanged += richTextBox1_TextChanged;
+            // 
+            // openFileDialogEditor
+            // 
+            openFileDialogEditor.FileName = "openFileDialog1";
+            openFileDialogEditor.Filter = "Archivo de Texto|*.txt";
+            openFileDialogEditor.FileOk += openFileDialogEditor_FileOk;
+            // 
+            // saveFileDialogEditor
+            // 
+            saveFileDialogEditor.Filter = "Archivo de Texto|*.txt";
+            // 
+            // frmEditor
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(richTextBox1);
+            Controls.Add(menuEditor1);
+            MainMenuStrip = menuEditor1;
+            Name = "frmEditor";
+            Text = "Nu";
+            menuEditor1.ResumeLayout(false);
+            menuEditor1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuEditor1;
+        private ToolStripMenuItem menuEditor;
+        private ToolStripMenuItem nuevoToolStripMenuItem;
+        private ToolStripMenuItem abrirToolStripMenuItem;
+        private ToolStripMenuItem guardarToolStripMenuItem;
+        private ToolStripMenuItem guardarComoToolStripMenuItem;
+        private ToolStripMenuItem salirToolStripMenuItem;
+        private RichTextBox richTextBox1;
+        private OpenFileDialog openFileDialogEditor;
+        private SaveFileDialog saveFileDialogEditor;
+    }
+}
---
+++ MiPrimerApp2025C/frmEditor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MiPrimerApp2025C
+{
+    public partial class frmEditor : Form
+    {
+        public frmEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado;
+            resultado = saveFileDialogEditor.ShowDialog();
+            if (resultado == DialogResult.OK)
+            {
+                string filePath = saveFileDialogEditor.FileName;
+                string texto = richTextBox1.Text;
+                try
+                {
+                    File.WriteAllText(filePath, texto);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al guardar el archivo: " + ex.Message);
+                }
+
+            }
+
+        }
+
+        private void openFileDialogEditor_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado;
+            resultado = openFileDialogEditor.ShowDialog();
+            if (resultado == DialogResult.OK)
+            {
+                string filePath = saveFileDialogEditor.FileName;
+                try
+                {
+                    string texto = File.ReadAllText(filePath);
+                    richTextBox1.Text = texto;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al abrir el archivo: " + ex.Message);
+                }
+            }
+        }
+    }
+}
